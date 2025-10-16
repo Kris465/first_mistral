ca99d5db6a8fe55abe8155ccb766af880d117f95
Durak2000 <fetisovtimur0@gmail.com>
2025-10-16 10:48:27 +0300

    some kind of test

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+from mistralai import Mistral
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+api_key = os.environ["TOKEN"]
+model = "mistral-large-latest"
+
+client = Mistral(api_key=api_key)
+
+chat_response = client.chat.complete(
+    model=model,
+    messages=[
+        {
+            "role": "user",
+            "content": "What is the best French cheese?",
+        },
+    ]
+)
+print(chat_response.choices[0].message.content)
